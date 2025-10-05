0764a041aed1b0b0a511551b4c0184b6f5d64bef
pybdrew <116611820+pybdrew@users.noreply.github.com>
2025-10-05 10:55:04 -1000

    Activity 5

---
+++ Activities/Activity 5/README.md
@@ -0,0 +1,25 @@
+# Activity 5
+- Andrew Rodriguez
+- 5 October 2025
+
+## Introduction
+
+> [!Important]  
+> Link to [Code](../../music/)  
+
+In this activity, I explored the fundamental structure of a React application and how its different parts work together to display content on a webpage. The goal was to understand how React uses components, props, and state to build interactive user interfaces. By examining the index.js and App.js files, I learned how React elements are imported, rendered, and connected to the HTML root element. This exercise also introduced the concept of JSX, which combines JavaScript and HTML-like syntax, making React components easier to read and maintain. The activity sets the foundation for creating more dynamic applications by preparing to manage state and use event handlers within components.
+
+## Screenshots
+
+![FirstCard](./images/1.png)
+*Figure 1: The original card created in part 1 of the activity*
+
+<br>
+
+![FinalProduct](./images/2.png)
+*Figure 2: The final product using state and props to render album information on cards*
+<br>
+
+## Conclusion
+
+Through this activity, I gained a deeper understanding of how React organizes and manages data within web applications. I learned the importance of using components to keep code organized and reusable, and how props and state work together to make components dynamic and interactive. Watching how state updates instantly change the displayed content in the browser showed me React’s efficiency and reactivity. By applying these concepts in the music application, I successfully rendered multiple album cards and improved their data management. This activity reinforced the core principles of modern frontend development, giving me a strong foundation for building more complex and data-driven React applications in the future.
---
+++ music/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Card from './Card'
+import './App.css';
+
+
+const App = () => {
+    const [albumList, setAlbumList] = useState ([
+        {
+            artistId: 0,
+            artist: 'The Beatles',
+            title: 'Yellow Submarine',
+            description: 
+                'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United Satted and on 17 January 1969 in the United Kingdom.',
+            year: 1969,
+            image: 
+                'https://upload.wikimedia.org/wikipedia/en/6/6f/Beatles_Yellow_Submarine_move_poster.jpg'
+        },
+        {
+            artistId: 1,
+            artist: 'The Beatles',
+            title: 'Abbey Road',
+            description: 
+                'Abbey Road is the eleventh studio album by English rock band The Beatles, released on 26 September 1969 by Apple Records. The recording sessions for the album were the last in which all four Beatles participated.',
+            year: '1969',
+            image: 
+                'https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg'
+        },
+        {
+            artistId: 2,
+            artist: 'The Beatles',
+            title: 'Let It Be',
+            description: 
+                'Let It Be is the twelfth and final studio album by the English rock band The Beatles. It was released on 8 May 1970, almost a month after the group\'s break-up',
+            year: 1970,
+            image: 
+                'https://upload.wikimedia.org/wikipedia/en/7/7a/The_Beatles_-_Let_It_Be.png'
+        },
+    ]);
+
+    const renderedList = () => {
+        return albumList.map((album) => {
+            return (
+                <Card
+                    key={album.artistId}
+                    albumTitle={album.title}
+                    albumDescription={album.description}
+                    buttonText='OK'
+                    imgURL={album.image} 
+                />
+            );
+        });
+    };
+
+    return <div className='container'>{renderedList()}</div>;
+};
+
+export default App;
